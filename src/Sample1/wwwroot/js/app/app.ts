8f2309cdc35317e081c85625c9d3442d7d5a7063
Kohinoor Basu <kbasu1@hotmail.com>
2016-11-10 15:48:04 -0800

    Commit 1

---
+++ src/Sample1/wwwroot/js/app/app.ts
@@ -0,0 +1,9 @@
+﻿class Startup {
+    public static main(): number {
+        console.log("Hello World!!, this is first typescript code");
+        return 0;
+    }
+}
+
+Startup.main();
+
---
+++ src/Sample1/wwwroot/scripts/app.js
@@ -0,0 +1,12 @@
+var Startup = (function () {
+    function Startup() {
+    }
+    Startup.main = function () {
+        console.log("Hello World!!, this is first typescript code");
+        return 0;
+    };
+    return Startup;
+}());
+Startup.main();
+//# sourceMappingURL=app.js.map
+// Write your Javascript code.
